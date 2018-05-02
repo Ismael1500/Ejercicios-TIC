e0661a7119cb0d40e8b81601432462f0ffdd79e1
Ismael1500 <33655572+Ismael1500@users.noreply.github.com>
2018-05-02 17:11:43 +0200

    Add files via upload

---
+++ Trabajo voluntario Areas/FigurasGeometricas.java
@@ -0,0 +1,6 @@
+
+interface FigurasGeometricas{//En la interface declaro lo que se debe hacer y cada implement decribira la logica de su comportamiento.
+    void area();
+}
+//Utilizaremos implements que obliga a la clase a implementar los métodos que estaban definidos en la interfaz.- 
+//-Como se puede ver cada clase implementa la interfaz a su manera.
